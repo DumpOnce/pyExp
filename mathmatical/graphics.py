9c690ab495acfdb833149c5ac518562e40205a8f
SuprDump <71075649+LangBucket@users.noreply.github.com>
2021-10-11 23:50:37 +0300

    Update graphics.py

--- mathmatical/graphics.py
+++ mathmatical/graphics.py
@@ -1,14 +1,18 @@
 from matplotlib import *
 import matplotlib.pyplot as plt
 import numpy
-
+import math
 
 def main():
     x = numpy.linspace(0, 100, 110)
     y = x**(-3)
     plt.title("linear function")
 
-    plt.plot(x, y)
+   # plt.plot(x, y)
+
+    #y2 = numpy.log(2, x)
+    y2 = [i**4 for i in x]
+    plt.plot(x, y, x, y2)
     plt.show()
 
 
